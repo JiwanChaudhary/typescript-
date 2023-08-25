99cc1b22e6479e1ab7cc2367bd9d91934d486d96
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-08-25 23:36:34 +0545

    functions part-2, void, never

--- functions.js
+++ functions.js
@@ -4,6 +4,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // function to add two numbers
 function addTwo(num) {
     return num + 2;
+    // return "hello";
 }
 // function that changes string to upper case
 function toUpper(val) {
@@ -20,3 +21,28 @@ addTwo(5);
 toUpper("Jiwan");
 signUpUser("jiwan", "jiwan@gmail.com", true);
 loginUser("j", "j@j.com");
+// another function
+// the following case can happen
+// function getValue(val: number) {
+//   if (val > 5) {
+//     return true;
+//   }
+//   return "200 OK";
+// }
+var getHello = function (s) {
+    return "";
+};
+var heros = [1, 2, 3];
+// const heros = ["thor", "hulk", "ironman"];
+heros.map(function (hero) {
+    return "hero is ".concat(heros);
+    //   this function must return an string
+    // return 1, will give error
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+    // return 1;, this will give error as function is of type void which means that this function doesnot returns anything
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
--- functions.ts
+++ functions.ts
@@ -1,12 +1,13 @@
 // in case of function we must decalare the type, so that we might not run in the error in the future
 
 // function to add two numbers
-function addTwo(num: number) {
+function addTwo(num: number): number {
   return num + 2;
+  // return "hello";
 }
 
 // function that changes string to upper case
-function toUpper(val: string) {
+function toUpper(val: string): string {
   return val.toUpperCase();
 }
 
@@ -21,5 +22,37 @@ addTwo(5);
 toUpper("Jiwan");
 signUpUser("jiwan", "jiwan@gmail.com", true);
 loginUser("j", "j@j.com");
+
+// another function
+// the following case can happen
+// function getValue(val: number) {
+//   if (val > 5) {
+//     return true;
+//   }
+//   return "200 OK";
+// }
+
+const getHello = (s: string): string => {
+  return "";
+};
+
+const heros = [1, 2, 3];
+// const heros = ["thor", "hulk", "ironman"];
+
+heros.map((hero): string => {
+  return `hero is ${heros}`;
+  //   this function must return an string
+  // return 1, will give error
+});
+
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+  // return 1;, this will give error as function is of type void which means that this function doesnot returns anything
+}
+
+
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
+}
 
 export {};
