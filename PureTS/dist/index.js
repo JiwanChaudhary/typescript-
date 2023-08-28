7ecbbaf792669df97f90e3a7a204ad7a23b13f90
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-08-29 01:42:08 +0545

    setup ts for projects

---
+++ PureTS/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("TypeScript is here");
+console.log("Hello");
+console.log("Good");
+console.log("a");
+console.log("adhgc");
+// tsc --init <- yo command ho, yesko help le tsconfig.json file generate huncha
+// tsc -w le terminal watchmode ma jancha
+// tsconfig.json file ma outDir select garna parcha so that ts file aafai js file ma convert huncha tyo destination folder bhitra euta file banayera
