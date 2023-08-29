f2fb1b11c81fbf55e81bfacf20ad20f36230840a
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-08-30 00:47:49 +0545

    getter and setter in typeScript

--- PureTS/dist/index.js
+++ PureTS/dist/index.js
@@ -14,7 +14,25 @@ class User {
         this.email = email;
         this.name = name;
         this.id = id;
+        this._courseCount = 1;
         this.city = "Saptari";
+    }
+    //?   private method vanne ni huncha, e.g. below cha, cannot be accessed outside class
+    deleteToken() { }
+    // getter starts with get Keyword
+    get getAppleId() {
+        return `apple email: ${this.email}`;
+    }
+    get CourseCount() {
+        return this._courseCount;
+    }
+    //   setter starts with set keyword and no return type and accepts a parameter
+    set CourseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be greater than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const Jiwan = new User("j@j.com", "jiw", "jujs");
+// Jiwan.deleteToken; , error dincha yesle
--- PureTS/src/index.ts
+++ PureTS/src/index.ts
@@ -10,8 +10,31 @@
 
 //? Better way to write above code
 class User {
+  private _courseCount = 1;
   private readonly city: string = "Saptari";
   constructor(public email: string, public name: string, private id: string) {}
+
+  //?   private method vanne ni huncha, e.g. below cha, cannot be accessed outside class
+  private deleteToken() {}
+
+  // getter starts with get Keyword
+  get getAppleId(): string {
+    return `apple email: ${this.email}`;
+  }
+
+  get CourseCount(): number {
+    return this._courseCount;
+  }
+
+  //   setter starts with set keyword and no return type and accepts a parameter
+  set CourseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be greater than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
 
 const Jiwan = new User("j@j.com", "jiw", "jujs");
+
+// Jiwan.deleteToken; , error dincha yesle
