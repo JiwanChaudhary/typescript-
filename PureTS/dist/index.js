972945cf9847aff038d3bccd6f2f394e503a9ef4
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-08-30 00:24:28 +0545

    classes

--- PureTS/dist/index.js
+++ PureTS/dist/index.js
@@ -1,9 +1,10 @@
 "use strict";
-console.log("TypeScript is here");
-console.log("Hello");
-console.log("Good");
-console.log("a");
-console.log("adhgc");
-// tsc --init <- yo command ho, yesko help le tsconfig.json file generate huncha
-// tsc -w le terminal watchmode ma jancha
-// tsconfig.json file ma outDir select garna parcha so that ts file aafai js file ma convert huncha tyo destination folder bhitra euta file banayera
+class User {
+    constructor(email, name) {
+        this.city = "";
+        this.email = email;
+        this.name = name;
+    }
+}
+const Jiwan = new User("j@j.com", "jiw");
+Jiwan.city = "Kathmandu";
--- PureTS/src/index.ts
+++ PureTS/src/index.ts
@@ -1,9 +1,12 @@
-console.log("TypeScript is here");
-console.log("Hello");
-console.log("Good");
-console.log("a");
-console.log("adhgc");
+class User {
+  email: string;
+  name: string;
+  city: string = "";
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
+}
 
-// tsc --init <- yo command ho, yesko help le tsconfig.json file generate huncha
-// tsc -w le terminal watchmode ma jancha
-// tsconfig.json file ma outDir select garna parcha so that ts file aafai js file ma convert huncha tyo destination folder bhitra euta file banayera
+const Jiwan = new User("j@j.com", "jiw");
+Jiwan.city = "Kathmandu";
