b6af0b4000e1b4fc8d474c68c3e4c958808927f8
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2023-08-30 00:34:54 +0545

    private and public variable inside class

--- PureTS/dist/index.js
+++ PureTS/dist/index.js
@@ -1,10 +1,20 @@
 "use strict";
+// class User {
+//   email: string;
+//   name: string;
+//   private readonly city: string = "Saptari"; // this is only accessible at this User class only, outside this is not accessible
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+//? Better way to write above code
 class User {
-    constructor(email, name) {
-        this.city = "";
+    constructor(email, name, id) {
         this.email = email;
         this.name = name;
+        this.id = id;
+        this.city = "Saptari";
     }
 }
-const Jiwan = new User("j@j.com", "jiw");
-Jiwan.city = "Kathmandu";
+const Jiwan = new User("j@j.com", "jiw", "jujs");
--- PureTS/src/index.ts
+++ PureTS/src/index.ts
@@ -1,12 +1,17 @@
+// class User {
+//   email: string;
+//   name: string;
+//   private readonly city: string = "Saptari"; // this is only accessible at this User class only, outside this is not accessible
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+//? Better way to write above code
 class User {
-  email: string;
-  name: string;
-  city: string = "";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
+  private readonly city: string = "Saptari";
+  constructor(public email: string, public name: string, private id: string) {}
 }
 
-const Jiwan = new User("j@j.com", "jiw");
-Jiwan.city = "Kathmandu";
+const Jiwan = new User("j@j.com", "jiw", "jujs");
